1b4df6f8a3a8414074a5aaa3336b71c68f9d0b12
Christian Ramos <christivn.ramos@gmail.com>
2024-02-12 21:27:58 +0100

    Update

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
 from domainInfo import *
 
-domain = "inventtatte.com"
+domain = "google.com"
 print(getDomainInfo(domain))
+
+#{'domain': 'google.com', 'ip': '172.217.17.14', 'asn': 'AS15169', 'org': 'GOOGLE', 'country_code': 'US', 'ns': ['ns3.google.com.', 'ns1.google.com.', 'ns2.google.com.', 'ns4.google.com.'], 'mx': ['smtp.google.com.']}
